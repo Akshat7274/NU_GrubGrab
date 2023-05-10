1cb95d204fcd6f150095c5b18b7113d44d56d3b0
gaytrisran03 <gaytrisran03@gmail.com>
2023-05-10 14:57:23 +0530

    fronte

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -10,27 +10,29 @@ const Contact = () => {
             alt="contactus"
             style={{ width: "100%"}}
           />
-          <div classname="col-sm-6">
-            <p className="text-center mt-2">
-              For any query and info about product displayed feel free to contact our team.
-            </p>
-            <p className="text-center mt-3">
-              <BiMailSend /> : nugrubgrab@gmail.com
-            </p>
-            <p className="text-center mt-3">
-              <BiPhoneCall /> : +91 12345 67890
-            </p>
-          </div>
-          <div classname="col-sm-6">
-            <p className="text-center mt-2">
-              For any food related query and info feel free to contact nescafe.
-            </p>
-            <p className="text-center mt-3">
-              <BiMailSend /> : xmemail@gmail.com
-            </p>
-            <p className="text-center mt-3">
-              <BiPhoneCall /> : +91 12345 67890
-            </p>
+          <div>
+            <div classname="col-md-4">
+              <p className="text-center mt-2">
+                For any query and info about product displayed feel free to contact our team.
+              </p>
+              <p className="text-center mt-3">
+                <BiMailSend /> : nugrubgrab@gmail.com
+              </p>
+              <p className="text-center mt-3">
+                <BiPhoneCall /> : +91 12345 67890
+              </p>
+            </div>
+            <div classname="col-md-4">
+              <p className="text-center mt-2">
+                For any food related query and info feel free to contact nescafe.
+              </p>
+              <p className="text-center mt-3">
+                <BiMailSend /> : xmemail@gmail.com
+              </p>
+              <p className="text-center mt-3">
+                <BiPhoneCall /> : +91 12345 67890
+              </p>
+            </div>
           </div>
       </div>
     </Layout>
