9ae9a7342aac0b5e7fd944927e4999f07170e006
Vaibhav Mishra <vaibhavc608@gmail.com>
2023-05-11 18:26:29 +0530

    push

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -6,8 +6,8 @@ const Categories = () => {
   const categories = useCategory();
   return (
     <Layout title={"All Categories"}>
-      <div className="container" style={{ marginTop: "100px" }}>
-        <div className="row container">
+      <div className="container" style={{ marginTop: "60px" }}>
+        <div className="row container" style={{padding:"5rem"}}>
           {categories.map((c) => (
             <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
               <div className="card">
--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -5,7 +5,7 @@ import { useAuth } from "../../context/auth";
 const Dashboard = () => {
   const [auth] = useAuth();
   return (
-    <Layout title={"Dashboard - Ecommerce App"}>
+    <Layout title={"Dashboard - NU GrubGrab"}>
       <div className="container-flui m-3 p-3 dashboard">
         <div className="row">
           <div className="col-md-3">
