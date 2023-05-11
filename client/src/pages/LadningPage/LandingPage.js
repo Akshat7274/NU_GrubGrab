b4f4a0e359b3b0efd4cc7b2c41ef7a0f599d85fd
gaytrisran03 <gaytrisran03@gmail.com>
2023-05-11 20:40:25 +0530

    ui

--- client/src/pages/LadningPage/LandingPage.js
+++ client/src/pages/LadningPage/LandingPage.js
@@ -10,30 +10,30 @@ const LandingPage = () => {
                 <div className='col-md-4 subdiv d-flex justify-content-center'>
                     <div className="card " style={{width: "30rem"}}>
                     
-                    <div className="card-body">
+                    <div className="card-bodyv1">
                         <h3 className="card-title mb-4 mt-2">NESCAFE</h3>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/nescafe" className="btn button mb-2 mt-4">Visit Page</a>
+                        <a href="/nescafe" className="btn btn-outline-dark mb-2 mt-4">Visit Page</a>
                     </div>
                     </div>
                 </div>
                 <div className="col-md-4 subdiv d-flex justify-content-center">
                     <div className="card" style={{width: "30rem"}}>
                     
-                    <div className="card-body">
+                    <div className="card-bodyv2">
                         <h3 className="card-title mb-4 mt-2 mb-4 mt-2">TERA MERA POINT</h3>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/coming-soon-tmp" className="btn button mb-2 mt-4">Visit Page</a>
+                        <a href="/coming-soon-tmp" className="btn btn-outline-dark mb-2 mt-4">Visit Page</a>
                     </div>
                     </div>
                 </div>
                 <div className="col-md-4 subdiv d-flex justify-content-center">
                     <div className="card" style={{width: "30rem"}}>
                     
-                    <div className="card-body">
+                    <div className="card-bodyv3">
                         <h3 className="card-title mb-4 mt-2">APNO GAON</h3>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/coming-soon-apno" className="btn button mb-2 mt-4">Visit Page</a>
+                        <a href="/coming-soon" className="btn button mb-2 mt-4">Visit Page</a>
                     </div>
                     </div>
                 </div>
@@ -41,9 +41,9 @@ const LandingPage = () => {
                     <div className="card" style={{width: "30rem"}}>
                     
                     <div className="card-body">
-                        <h3 className="card-title mb-4 mt-2">Silver Spoons</h3>
+                        <h3 className="card-title mb-4 mt-2">SILVER SPOONS</h3>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/coming-soon-silver" className="btn button mb-2 mt-4">Visit Page</a>
+                        <a href="/coming-soon" className="btn button mb-2 mt-4">Visit Page</a>
                     </div>
                     </div>
                 </div>
