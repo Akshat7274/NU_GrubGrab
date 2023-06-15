40db8a3554751591ceafe61dd569a38f7f7f8fb7
Atharv2502 <atharv.deogaonkar01@gmail.com>
2023-06-15 12:41:03 +0530

    Payment Gateway Controller Updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -22,7 +22,7 @@ export const createProductController = async (req, res) => {
     const { name, description, price, category, quantity, shipping } =
       req.fields;
     const { photo } = req.files;
-    //alidation
+    //Validation
     switch (true) {
       case !name:
         return res.status(500).send({ error: "Name is Required" });
