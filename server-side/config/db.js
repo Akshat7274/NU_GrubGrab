c8aff3c48a84cc87713e7c35590d8bc6730dfbb0
Akshat7274 <akshat1205aj@gmail.com>
2023-11-30 05:18:25 +0530

    Database Segregation Setup Basic

--- server-side/config/db.js
+++ server-side/config/db.js
@@ -4,10 +4,10 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      `Connected To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Error in Mongodb ${error}`.bgRed.white);
   }
 };
 
