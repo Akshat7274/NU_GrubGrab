56964c754e7ea432e67ed7a40c6c68c6f0366e5f
Akshat7274 <akshat1205aj@gmail.com>
2023-12-02 11:23:03 +0530

    Selenium Dummy Test Added

--- server-side/selenium.js
+++ server-side/selenium.js
@@ -1,9 +1,9 @@
-import { Builder, By, Key } from "selenium-webdriver"
+import { Builder, By, Key} from "selenium-webdriver"
 
 async function tester(){
     let browser = await new Builder().forBrowser('chrome').build();
-    await browser.get('https://www.google.com/')
-    await browser.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]")).sendKeys("test this",Key.RETURN)
+    await browser.get('https://www.bing.com/')
+    await browser.findElement(By.id("sb_form_q")).sendKeys("test this",Key.RETURN)
     await browser.quit()
 }
 
