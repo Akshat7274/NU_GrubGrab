55adc763a8028144e164ef8e4e80d866bce4442e
Ze-Leus <71334938+Akshat7274@users.noreply.github.com>
2023-12-02 13:20:31 +0530

    Added Selenium for NU GrubGrab App

--- server-side/selenium.js
+++ server-side/selenium.js
@@ -1,9 +1,14 @@
+import assert from "assert";
 import { Builder, By, Key} from "selenium-webdriver"
 
 async function tester(){
     let browser = await new Builder().forBrowser('chrome').build();
-    await browser.get('https://www.bing.com/')
-    await browser.findElement(By.id("sb_form_q")).sendKeys("test this",Key.RETURN)
+    await browser.get('http://localhost:3000')
+    await browser.findElement(By.xpath("/html/body/div/div/main/div[2]/div/div[2]/div/div/div[1]/a")).click()
+    let item = await browser.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div[2]/div[1]/div[1]/div/div[1]/h5[1]")).getText().then(function (value){
+        return value
+    })
+    assert.strictEqual(item,"Contc. Pizza Large")
     await browser.quit()
 }
 
