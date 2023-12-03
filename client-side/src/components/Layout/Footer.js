664401dea14c6489d2a2bc286e517c5cdb9d2e1a
gaytrisran03 <gaytrisran03@gmail.com>
2023-12-03 13:07:52 +0530

    footer update

--- client-side/src/components/Layout/Footer.js
+++ client-side/src/components/Layout/Footer.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1 className="text-center">All Right Reserved &copy; NU GrubGrab</h1>
-      <h4 classname="text-center">For any queries contact: nugrubgrab@gmail.com</h4>
-      <p className="text-center mt-3">
+      <h5 className="text-center">All Right Reserved &copy; NU GrubGrab</h5>
+      <h6 classname="text-center">For any queries contact: nugrubgrab@gmail.com</h6>
+      <p className="text-center mt-6">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>
       </p>
