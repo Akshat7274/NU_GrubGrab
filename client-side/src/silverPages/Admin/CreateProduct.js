3f50eb8fa8be725732c7f3a1e14c444db8b97297
Atharv2502 <atharv.deogaonkar01@gmail.com>
2023-12-04 11:25:47 +0530

    Route Updates

--- client-side/src/silverPages/Admin/CreateProduct.js
+++ client-side/src/silverPages/Admin/CreateProduct.js
@@ -54,7 +54,7 @@ const CreateProduct = () => {
         toast.error(data?.message);
       } else {
         toast.success("Product Created Successfully");
-        navigate("/dashboard/admin/products");
+        navigate("/dashboard/silver-spoon/admin/products");
       }
     } catch (error) {
       console.log(error);
